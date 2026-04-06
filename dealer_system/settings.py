c0676f3d34b9f809fdb9da24bad0d69965acc4b3
Varun <dvarun2020@gmail.com>
2026-04-06 12:01:44 +0530

    fix os import

--- dealer_system/settings.py
+++ dealer_system/settings.py
@@ -1,3 +1,4 @@
+
 """
 Django settings for dealer_system project.
 
@@ -10,6 +11,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -127,7 +129,7 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
-import os
+
 
 DEBUG = os.getenv("DEBUG", "True") == "True"
 
